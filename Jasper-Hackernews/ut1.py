0547c2bff454b48cbf9c87381f08a2d0fd1df6a6
priya <priya99920@gmail.com>
2014-12-07 12:19:22 +0530

    File renamed to Hacker_News

--- Jasper-Hackernews/ut1.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from jasper import latestStories
-
-class JasperTestCase(unittest.TestCase):
-    def test_latestStories(self):
-        self.assertTrue(latestStories())
-if __name__ == '__main__':
-    unittest.main()
