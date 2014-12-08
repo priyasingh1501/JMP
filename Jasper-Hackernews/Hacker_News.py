746ca87572f4d4859ba33ec59f79c17d635b0152
priya <priya99920@gmail.com>
2014-12-08 20:04:04 +0530

    Adding Documentation

--- Jasper-Hackernews/Hacker_News.py
+++ Jasper-Hackernews/Hacker_News.py
@@ -1,8 +1,28 @@
+"""
+This module demonstrates the HackerNews module that will be used by jasper
+to provide a voice output to a particular voice input.It provides the current
+top 10 stories as the output whenever it gets an input that contains the following
+words-Stories,from,hackernews.
+
+Example:
+
+Audio Input:
+Jasper. Give me the top 10 stories from hackernews.
+
+Audio Output:
+Your top 10 stories are ---
+
+Attributes and functions:
+HN is a class in the HackerNews API that includes all the
+functions that are working in the program.hn is an object of the class HN that
+is used to call the functions of HN.
+"""
+
 import re
 from hn import HN
 
-hn = HN()
-         
+
+hn = HN()         
 
 WORDS = ["STORIES","FROM","HACKERNEWS"];
 
@@ -10,7 +30,7 @@ def isValid(text):
     return bool(re.search(r'\bstories from hackernews\b', text, re.IGNORECASE))
 
 def handle(text, mic, profile):
-    mic.say("Hi.The top 10 stories are as follows: ");
+    mic.say("Your top 10 stories are ");
     for story in hn.get_stories(limit=10):
         s_title = story.title;
         s_rank=story.rank;
